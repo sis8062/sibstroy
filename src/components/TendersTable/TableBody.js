1f20e3d3b5fc874effd25f247736afe2e687abd2
Arthur Malumian <amalumian@outlook.com>
2023-11-07 19:05:45 +0700

    Initial commit

---
+++ src/components/TendersTable/TableBody.js
@@ -0,0 +1,33 @@
+import classes from "./TableBody.module.scss";
+
+const TableBody = ({ tenders, onShow }) => {
+  const showTenderHandler = (e) => {
+    onShow(true, e.currentTarget.id);
+  };
+
+  const tendersRow = tenders.map((tender) => (
+    <tr
+      className={classes.table__row}
+      id={tender.id}
+      key={tender.id}
+      onClick={showTenderHandler}
+    >
+      <td data-cell="Предмет" className={classes.table__data}>
+        {tender.subject}
+      </td>
+      <td data-cell="Тип" className={classes.table__data}>
+        {tender.type}
+      </td>
+      <td data-cell="Начало" className={classes.table__data}>
+        {tender.start}
+      </td>
+      <td data-cell="Окончание" className={classes.table__data}>
+        {tender.end}
+      </td>
+    </tr>
+  ));
+
+  return <tbody className={classes.table__body}>{tendersRow}</tbody>;
+};
+
+export default TableBody;
